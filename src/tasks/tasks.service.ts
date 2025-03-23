a745c176b79982238e876c2ddd46577a1363963e
giorgi <giorgi.sikh@outlook.com>
2025-03-23 14:00:28 +0400

    add paginatination in task API #72

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,6 +9,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateTaskLabelDto } from './create-task-label.dto';
 import { TaskLabel } from './task-label.entity';
 import { FindTaskParams } from './find-task.params';
+import { PaginationParams } from 'src/common/pagination.params';
 
 @Injectable()
 export class TasksService {
@@ -19,12 +20,18 @@ export class TasksService {
     private readonly labelsRepository: Repository<TaskLabel>,
   ) {}
 
-  public async findAll(filters: FindTaskParams): Promise<Task[]> {
-    return await this.taskRepository.find({
+  public async findAll(
+    filters: FindTaskParams,
+    pagination: PaginationParams
+
+  ): Promise<[Task[], number]> {
+    return await this.taskRepository.findAndCount({
       where: {
-        status: filters.status        
+        status: filters.status,        
       },
-      relations: ['labels']
+      relations: ['labels'],
+      skip: pagination.offset,
+      take: pagination.limit
     });
   }
 

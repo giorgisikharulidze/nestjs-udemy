df4ad0862e1fa086dd668c32ea314aeea8ff76ed
giorgi <giorgi.sikh@outlook.com>
2025-03-24 18:59:31 +0400

    create user service

---
+++ src/users/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import { PasswordService } from '../password/password.service';
+import { CreateUserDto } from '../create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly userRepository: Repository<User>,
+    private readonly passwordService: PasswordService,
+  ) {}
+
+  // 1) find the user by email
+  // 2) create user
+  // 3) fetch the user by id
+
+  public async findOneByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOneBy({ email });
+  }
+
+  public async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const hashPassword = await this.passwordService.hash(
+      createUserDto.password,
+    );
+    const user = this.userRepository.create({
+      ...createUserDto,
+      password: hashPassword,
+    });
+
+    return await this.userRepository.save(user);
+  }
+
+  public async findOne(id: string): Promise<User | null>{
+
+    return await this.userRepository.findOneBy({id});
+  }
+  
+}

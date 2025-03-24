25a4d2fc3328e95f5e96f9537c076ba601463360
giorgi <giorgi.sikh@outlook.com>
2025-03-24 18:44:03 +0400

    #90, add test end section #7

--- src/users/password/password.service.spec.ts
+++ src/users/password/password.service.spec.ts
@@ -29,12 +29,27 @@ describe('PasswordService', () => {
   // bcrypt.hash  -> was called
   //              -> password was passed to it & salt rounds
   // mocks & spies
-  it('should hash password',async()=>{
+  it('should hash password', async () => {
     const mockHash = 'hashed_password';
     (bcrypt.hash as jest.Mock).mockResolvedValue(mockHash);
-    const password = 'password123' 
+    const password = 'password123';
     const result = await service.hash(password);
-    expect(bcrypt.hash).toHaveBeenCalledWith(password,10);
+    expect(bcrypt.hash).toHaveBeenCalledWith(password, 10);
     expect(result).toBe(mockHash);
-  })
+  });
+
+  it('should fail on inccorect password', async () => {
+    // 1) mock bcrypt.compare() -  fails
+    // 2) mock the resolved value
+    // 3) Call the service method - verify()
+    // 4) bcrypt.compare - was called wit specific arguments
+    // 5) We verify if the service method returned what bcrypt.compare did
+
+    const mockHash = 'hashed_password';
+    const plainPassword = 'password123';
+    (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+    const result = await service.verify(plainPassword, mockHash);
+    expect(bcrypt.compare).toHaveBeenCalledWith(plainPassword, mockHash);
+    expect(result).toBe(true);
+  });
 });

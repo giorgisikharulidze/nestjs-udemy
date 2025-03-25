b846638a4b64be53debef887037c1ad1b9341755
giorgi <giorgi.sikh@outlook.com>
2025-03-25 11:43:45 +0400

    adding protect profile

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -1,30 +1,55 @@
-import { Body, ClassSerializerInterceptor, Controller, Post, SerializeOptions, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  Request,
+  SerializeOptions,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserDto } from '../create-user.dto';
 import { AuthService } from './auth.service';
 import { User } from '../user.entity';
 import { LoginDto } from '../login.dto';
 import { LoginResponse } from '../login.response';
+import { UserService } from '../user/user.service';
 import { use } from 'passport';
+import { AuthRequest } from './auth.request';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
-@SerializeOptions({strategy: 'excludeAll'})
+@SerializeOptions({ strategy: 'excludeAll' })
 export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
-    constructor (
-        private readonly authService: AuthService,
-    ){}
-    
-    @Post('register')   
-    public async register(@Body() createUserDto: CreateUserDto):Promise<User>{
-        const user = await this.authService.register(createUserDto); 
-        return user;
- }   
+  @Post('register')
+  public async register(@Body() createUserDto: CreateUserDto): Promise<User> {
+    const user = await this.authService.register(createUserDto);
+    return user;
+  }
 
-    @Post('login')
-    public async login(@Body() loginDto: LoginDto):Promise<LoginResponse>{
-        const accessToken =  await this.authService.login(loginDto.email,loginDto.password);
+  @Post('login')
+  public async login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
+    const accessToken = await this.authService.login(
+      loginDto.email,
+      loginDto.password,
+    );
 
-        return  new LoginResponse({accessToken});
+    return new LoginResponse({ accessToken });
+  }
+
+  @Get('/profile')
+  async profile(@Request() request: AuthRequest): Promise<User> {
+    const user = await this.userService.findOne(request.user.sub);
+
+    if (user) {
+      return user;
     }
+    throw new NotFoundException();
+  }
 }

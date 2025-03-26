5ead898ef1e2b599526f6b3667405d2f11fcb1d4
giorgi <giorgi.sikh@outlook.com>
2025-03-26 12:57:53 +0400

    add exeption part(process.exit) if crash application

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+ // await app.listen(process.env.PORT ?? 3000);
+
+  try {
+    await app.listen(process.env.PORT ?? 3000);
+    console.log(`Application successfully started on port ${process.env.PORT ?? 3000}`);
+  } catch (error) {
+    console.error(`Failed to start application on port ${process.env.PORT ?? 3000}. Error:`, error.message);
+  }
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);  // თუ რამე შეცდომაა, აპლიკაცია დაიხურება
+});

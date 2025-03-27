fa74d302cf72d65dfa549d4c97cace7fc05f8da2
giorgi <giorgi.sikh@outlook.com>
2025-03-27 23:06:29 +0400

    add paginator function

---
+++ src/common/paginator.ts
@@ -0,0 +1,27 @@
+import { SelectQueryBuilder } from 'typeorm';
+import { PaginationResponse } from 'src/common/pagination.response';
+import { PaginationParams } from 'src/common/pagination.params';
+import { ObjectLiteral } from 'typeorm';
+
+/**
+ * პაგინაციის გენერიკული ფუნქცია TypeORM-ის QueryBuilder-სთვის.
+ */
+export async function paginate<T extends ObjectLiteral>(
+  qb: SelectQueryBuilder<T>,
+  pagination: PaginationParams,
+): Promise<PaginationResponse<T>> {
+  // პაგინაციის პარამეტრების დამატება
+  qb.skip(pagination.offset).take(pagination.limit);
+
+  // შედეგების წამოღება
+  const [data, total] = await qb.getManyAndCount();
+
+  return {
+    data,
+    meta: {
+      total,
+      offset: pagination.offset,
+      limit: pagination.limit,
+    },
+  };
+}
